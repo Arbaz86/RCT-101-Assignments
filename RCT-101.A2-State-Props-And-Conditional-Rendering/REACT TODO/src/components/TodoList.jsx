b804456c21f828eb1e4e7aa2d26f62634d187570
Arbaz86 <arbaz10042002@gmail.com>
2022-05-19 01:12:27 +0530

    completed todo assignment

---
+++ RCT-101.A2-State-Props-And-Conditional-Rendering/REACT TODO/src/components/TodoList.jsx
@@ -0,0 +1,60 @@
+import { TodoItem } from "./TodoItem";
+import { useState } from "react";
+import "./TodoList.css";
+
+export const TodoList = () => {
+  const [text, setText] = useState("");
+  const [todos, setTodos] = useState([]);
+
+  const onDelete = (id) => {
+    setTodos(todos.filter((todo) => todo.id !== id));
+  };
+
+  const handleChange = (e) => {
+    setText(e.target.value);
+    console.log(e.target.value);
+  };
+
+  const handleTodo = () => {
+    setTodos([...todos, { id: Date.now(), value: text, isCompleted: false }]);
+    setText("");
+  };
+
+  const onKeyPress = (e) => {
+    if (text !== "") {
+      if (e.key === "Enter") {
+        setTodos([
+          ...todos,
+          { id: Date.now(), value: text, isCompleted: false },
+        ]);
+        setText("");
+      }
+    }
+  };
+  return (
+    <>
+      <h1 className="todo-heading">What's the Plan for Today?</h1>
+      <input
+        className="todo-input"
+        value={text}
+        type="text"
+        placeholder="Add a todo"
+        onChange={handleChange}
+        onKeyPress={onKeyPress}
+      />
+      <button
+        className="btn-todo"
+        disabled={text === "" ? true : false}
+        onClick={handleTodo}
+      >
+        Add Todo
+      </button>
+
+      <div>
+        {todos.map((todo) => (
+          <TodoItem key={todo.id} todo={todo} onDelete={onDelete} />
+        ))}
+      </div>
+    </>
+  );
+};
